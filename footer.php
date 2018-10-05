758e34c36b2001a7919dbcec5977e7cb6d71bfda
Kevin Michael Gray <>
2018-10-05 11:10:12 -0700

    Footer Tracking Code

--- footer.php
+++ footer.php
@@ -65,4 +65,77 @@ wp_footer();
 ?>
 
 </body>
+
+    <!-- Perfect Audience Tracking -->
+<script type="text/javascript">
+  (function() {
+    window._pa = window._pa || {};
+    // _pa.orderId = "myOrderId"; // OPTIONAL: attach unique conversion identifier to conversions
+    // _pa.revenue = "19.99"; // OPTIONAL: attach dynamic purchase values to conversions
+    // _pa.productId = "myProductId"; // OPTIONAL: Include product ID for use with dynamic ads
+    
+    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+    pa.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + "//tag.marinsm.com/serve/5535e5586ff020e95f00009c.js";
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(pa, s);
+  })();
+</script>
+<!-- End of Perfect Audience Tracking -->
+    
+    <!-- Lucky Orange -->
+<script type='text/javascript'>
+window.__lo_site_id = 59972;
+
+	(function() {
+		var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+		wa.src = 'https://d10lpsik1i8c69.cloudfront.net/w.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+	  })();
+	</script><!-- End of Lucky Orange --> 
+ <!-- Active Campaign Event Tracking -->
+<?php
+$curl = curl_init("https://trackcmp.net/event");
+//curl_setopt($curl, CURLOPT_URL, "https://trackcmp.net/event"); 
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, array(
+    "actid" => "648870935",
+    "key" => "c5e8352989b7a99cf0a7f58b38290873266235c5",
+    "event" => "__pagevisit",
+    "eventdata" => "Page Visit",
+    "visit" => json_encode(array(
+        // If you have an email address, assign it here.
+            "email" => "",
+            )),
+        ));
+
+$result = curl_exec($curl);
+if ($result !== false) {
+    $result = json_decode($result);
+    if ($result->success) {
+        echo '<div style="display:none;">Success!</div>';
+    } else {
+        echo '<div style="display:none;">Error!</div>';
+    }
+
+} else {
+    echo 'cURL failed to run: ', curl_error($curl);
+}
+?>
+<!-- End of Active Campaign -->
+<!-- Start Active Campaign Site Tracking -->
+<script type="text/javascript">
+	var trackcmp_email = '';
+	var trackcmp = document.createElement("script");
+	trackcmp.async = true;
+	trackcmp.type = 'text/javascript';
+	trackcmp.src = '//trackcmp.net/visit?actid=648870935&e='+encodeURIComponent(trackcmp_email)+'&r='+encodeURIComponent(document.referrer)+'&u='+encodeURIComponent(window.location.href);
+	var trackcmp_s = document.getElementsByTagName("script");
+	if (trackcmp_s.length) {
+		trackcmp_s[0].parentNode.appendChild(trackcmp);
+	} else {
+		var trackcmp_h = document.getElementsByTagName("head");
+		trackcmp_h.length && trackcmp_h[0].appendChild(trackcmp);
+	}
+</script>
+<!-- End of Active Campaign -->
 </html>
