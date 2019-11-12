480991d0d0381b3978ee9b47718ae1c4de2f0e27
Stephen Gravitt <Stephen@Grav-IT.net>
2019-11-11 17:28:50 -0700

    live from esignature site

--- functions.php
+++ functions.php
@@ -59,5 +59,56 @@ add_action('generate_after_entry_content', 'wpb_author_info_box');
 remove_filter('pre_user_description', 'wp_filter_kses');
 
 
+
+if (function_exists('add_theme_support')) {
+  register_nav_menu('legal_disclaimer2', 'Legal Disclaimer Navigation2');
+}
+
+
+if ( ! function_exists( 'generate_construct_footer' ) ) {
+    
+        
+	add_action( 'generate_footer', 'generate_construct_footer' );
+	/**
+	 * Build our footer.
+	 *
+	 * @since 1.3.42
+	 */
+	function generate_construct_footer() {
+		?>
+		<footer class="site-info" itemtype="https://schema.org/WPFooter" itemscope="itemscope">
+			<div class="inside-site-info <?php if ( 'full-width' !== generate_get_setting( 'footer_inner_width' ) ) : ?>grid-container grid-parent<?php endif; ?>">
+				<?php
+				/**
+				 * generate_before_copyright hook.
+				 *
+				 * @since 0.1
+				 *
+				 * @hooked generate_footer_bar - 15
+				 */
+				do_action( 'generate_before_copyright' );
+				?>
+				<div class="copyright-bar">
+                                    
+                                    Approve Me™, 2443 Fillmore St #380-7600 San Francisco, CA 94115 USA<br />
+				Copyright © <?php echo date("Y"); ?> ApproveMe.  All Rights Reserved.<br />
+					<?php
+					/**
+					 * generate_credits hook.
+					 *
+					 * @since 0.1
+					 *
+					 * @hooked generate_add_footer_info - 10
+					 */
+					wp_nav_menu(array('theme_location' => 'legal_disclaimer2', 'container_id' => 'legal-disclaimer-nav', 'items_wrap' => "\n" . '<ul id="%1$s" class="%2$s">' . "\n" . '%3$s</ul>' . "\n"));
+					?>
+				</div>
+			</div>
+		</footer><!-- .site-info -->
+		<?php
+	}
+}
+
+
 // Add custom sidebar
 include("custom-sidebar.php");
